6192fa2bffe3463d807080fec3635f834a2f9cc9
Paulo <paulo.silveira@justdigital.com.br>
2015-09-09 15:59:08 -0300

    trofeu no ranking

--- client/ranking/ranking.js
+++ client/ranking/ranking.js
@@ -3,3 +3,17 @@ Template.ranking.helpers({
 		return UserRanking.find({}, {sort: {count: -1}});
 	}
 });
+
+Template.rankingRow.helpers({
+	prize: function () {
+		if (this.pos <= 3) {
+			if (this.pos == 1) {
+				return 'gold';
+			} else if (this.pos == 2) {
+				return 'silver';
+			} else if (this.pos == 3) {
+				return 'bronze';
+			}
+		}
+	}
+});
